0ba2057163e3cb63c493d8172a001677bada8d00
Felix Martinez Alvaro <Felix.Martinez@tajamar365.com>
2015-12-07 18:00:30 +0100

    Con BaseRepositorio

---
+++ BaseRepositorio/BaseRepositorio/Repositorio/IRepositorio.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using BaseRepositorio.ViewModel;
+
+namespace BaseRepositorio.Repositorio
+{
+    public interface IRepositorio<TModelo, TViewModel> where TModelo : class
+                         where TViewModel : IViewModel<TModelo>
+    {
+        TViewModel Add(TViewModel model);
+        int Borrar(int id);
+        int Borrar(Expression<Func<TModelo, bool>> consulta);
+        int Actualizar(TViewModel model);
+        ICollection<TViewModel> Get();
+        TViewModel Get(params Object[] keys);
+        ICollection<TViewModel> Get(Expression<Func<TModelo, bool>> consulta);
+
+    }
+
+}
+
+
